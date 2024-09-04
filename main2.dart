afd3dabe5268dc6155d0f07cd63d3667d8b0bde0
Muzammil-Khanani <Muzammilkhanani32@gmail.com>
2024-09-04 11:14:19 -0700

    ASSIGNMENT NO 3

--- main2.dart
+++ main2.dart
@@ -85,6 +85,7 @@ void main() {
   print(
       "Q.8: remove all false values from below list by using removeWhere or retainWhere property.List<String> usersEligibility = ['John', 'Alice', 'eligible', 'Mike', 'Sarah', 'Tom']?");
   List usersEligibility = ['John', 'Alice', 'eligible', 'Mike', 'Sarah', 'Tom'];
-  usersEligibility.removeWhere((usersEligibility) => usersEligibility == false);
+  usersEligibility
+      .removeWhere((usersEligibility) => usersEligibility != 'eligible');
   print(usersEligibility);
 }
